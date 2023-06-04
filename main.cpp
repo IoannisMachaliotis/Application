2ce27a8b1f61b5ea8c0e19f7a7eea5ddfb9ae18e
IoannisMachaliotis <johnmax1996@gmail.com>
2023-06-04 19:12:40 +0200

    Added main.cpp to Application repo

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* This is an Application Requested from ALTEN SA 
+    on behalf of a client in Montpellier 
+    @author imachaliotis
+*/
+
+#include "application.cpp"
+
+int main()
+{
+    std::cout << "Hello" << "\n";
+    Application();
+    return 0;
+} 
